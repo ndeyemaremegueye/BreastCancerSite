122af20648b925ba7cf01da8d04750bcfa2775d5
ndeyemaremegueye <maremegueye0308@gmail.com>
2023-06-21 13:38:19 +0000

    modifications:accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -10,45 +10,41 @@ import { NgIf } from '@angular/common';
   ]
 })
 export class AccueilComponent implements OnInit {
+  handleCarouselEvents(event: any) {
+    console.log(event);
+  }
+  ngOnInit(): void {
+  }
 
-  // constructor() { }
-  // currentIndex = 0;
-  // images = [
-  //   'assets/images/femme1.png',
-  //   'assets/images/femme.png',
-  //   'assets/images/femme1.png'
-  // ];
-
-  // nextSlide() {
-  //   this.currentIndex = (this.currentIndex + 1) % this.images.length;
-  // }
-
-  // previousSlide() {
-  //   this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
-  // }
-
-  carouselItems = [
-    {
-      image: 'assets/images/femme1.png',
-      caption: 'Image 1',
-      description: 'Description de l\'image 1'
-    },
-    {
-      image: 'assets/images/femme.png',
-      caption: 'Image 2',
-      description: 'Description de l\'image 2'
-    },
-    {
-      image: 'assets/images/femme1.png',
-      caption: 'Image 3',
-      description: 'Description de l\'image 3'
-    }
-  ];
+  images = [
+    {src: '/assets/breast-cancer.png'},
+    {name:'femme.png'},
+    {name:'femme1.png'}
+  ]
 
   
 
-  ngOnInit(): void {
-  }
+  // carouselItems = [
+  //   {
+  //     image: 'assets/images/femme1.png',
+  //     caption: 'Image 1',
+  //     description: 'Description de l\'image 1'
+  //   },
+  //   {
+  //     image: 'assets/images/femme.png',
+  //     caption: 'Image 2',
+  //     description: 'Description de l\'image 2'
+  //   },
+  //   {
+  //     image: 'assets/images/femme1.png',
+  //     caption: 'Image 3',
+  //     description: 'Description de l\'image 3'
+  //   }
+  // ];
+
+  
+
+  
 
 }
 
